90813b169e31213434784c8fabc8c88514d5ced8
Dima <budya2012@gmail.com>
2021-03-10 14:39:21 +0300

    fead: implement main function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseTemp = n.toString().split('');
+    let isMinus = reverseTemp[0] === '-';
+    reverseTemp = reverseTemp.reverse().join('');
+
+    if (isMinus) {
+        return reverseTemp.slice(0, -1);
+    }
+
+    return reverseTemp;
 }
